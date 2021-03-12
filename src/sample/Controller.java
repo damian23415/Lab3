9a9470748c49f0e2c97ce8009c956220d42005b5
Damian <damian.radomski9908@gmail.com>
2021-03-12 16:34:41 +0100

    Lab3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public Button btnMain;
+    public TextField txtLicznik;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        txtLicznik.setText("Kliknięcie nr: " + ++clickCounter);
+    }
 }
